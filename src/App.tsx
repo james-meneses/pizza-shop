8777237e3ce2f8ff993eacd451ded13e62878a20
James Meneses <james.meneseswk@gmail.com>
2024-10-31 11:06:35 -0300

    Creating app layout header and setting a theme provider with theme toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./routes";
 import { HelmetProvider, Helmet } from "react-helmet-async";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "./components/theme/theme-provider";
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster />
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster />
+      </ThemeProvider>
     </HelmetProvider>
   );
 }
